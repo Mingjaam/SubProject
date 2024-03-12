e673593d9561a4d9e73164bb3a28202fe1fc5059
mjmac <mjmac@E0C9>
2024-03-12 10:12:29 +0900

    5701576 김민재 3/12

--- test/main.c
+++ test/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-int sub(int a, int b) {
-	return a - b;
-}
-int main() {
-	int result = 0;
-	result = sub(2, 3);
-	printf("%d\n", result);
-	system("pause");
+
+int main(void) {
+
+	int i, j;
+	int sum = 2;
+
+	for (i = 3; i <= 100; i++) {
+		
+		for (j = i-1; j >= 1; j--) {
+
+			if (j == 1) { sum += i ; }
+			else if (i % j == 0) { break; }
+
+		}
+
+	}
+
+	printf("%d", sum);
+
 	return 0;
 }
-//commit
+
+
+
